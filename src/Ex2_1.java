9e96dd2ef7f20ef8ddfdf48e47ccbc94461aba89
Eumsoli <eumsoli@gmail.com>
2022-03-17 19:16:44 +0900

    fisrt commit

---
+++ src/Ex2_1.java
@@ -0,0 +1,19 @@
+
+public class Ex2_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//com alt A 멀티컬럼 편집
+
+		// println -> 프린트후에 줄바꿈
+		// print -> 프린트 후에 줄안바꿈
+		
+		System.out.println(5+3); //덧셈 
+		System.out.println(5-3); //뺄셈 
+		System.out.println(5*3); //곱셈 
+		System.out.println(5/3.0); //나눗셈 
+		
+	}
+
+}
